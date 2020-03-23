82d30108a668dcec103fffd28a7aac07ef1b7d6a
1355081829@qq.com <1355081829aA>
2020-03-23 12:16:14 +0800

    增加echarts统计组件

---
+++ src/components/Echarts.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :id="chartId" :style="{width: '100%', height: '300px'}"></div>
+</template>
+
+<script>
+    import echarts from 'echarts'
+
+    export default {
+        name: "Echarts",
+        props: ['chartId', 'option'],
+        mounted() {
+            this.drawLine();
+        },
+        methods: {
+            drawLine() {
+                // 基于准备好的dom，初始化echarts实例
+                let myChart = echarts.init(document.getElementById(this.chartId));
+                // 绘制图表
+                myChart.setOption(this.option);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/dashboard/Index.vue
+++ src/pages/dashboard/Index.vue
@@ -18,12 +18,57 @@
                 </el-card>
             </el-col>
         </el-row>
+        <el-row :gutter="12">
+            <el-col :span="12">
+                <el-card shadow="always">
+                    <echarts chart-id="user" :option="userOptions"></echarts>
+                </el-card>
+            </el-col>
+            <el-col :span="12">
+                <el-card shadow="always">
+                    <echarts chart-id="name" :option="nameOptions"></echarts>
+                </el-card>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
 <script>
+    import Echarts from '../../components/Echarts'
+
     export default {
-        name: "DashboardIndex"
+        name: "DashboardIndex",
+        components: {Echarts},
+        data() {
+            return {
+                userOptions: {
+                    title: {text: '新增用户统计'},
+                    tooltip: {},
+                    xAxis: {
+                        data: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '人数',
+                        type: 'bar',
+                        data: [5, 20, 36, 10, 10, 20, 5, 20, 36, 10, 10, 20]
+                    }]
+                },
+                nameOptions: {
+                    title: {text: '访问量统计'},
+                    tooltip: {},
+                    xAxis: {
+                        data: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '人次',
+                        type: 'bar',
+                        data: [500, 2000, 3600, 1000, 1000, 2000, 5000, 2000, 3600, 1000, 1990, 2000]
+                    }]
+                }
+            }
+        }
     }
 </script>
 
